ee0368effadc2f10b0aab4e684c1e8ca48095e2f
Shashi Dhar Kumar <11044107+shashidharkumar@users.noreply.github.com>
2018-08-24 21:32:32 +0530

    Create registeruser.sql

---
+++ registeruser.sql
@@ -0,0 +1,27 @@
+--
+-- Table structure for table `registeruser`
+--
+
+CREATE TABLE `registeruser` (
+  `id` int(11) NOT NULL,
+  `mobile` int(11) NOT NULL,
+  `mobiletoken` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `registeruser`
+--
+ALTER TABLE `registeruser` ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `registeruser`
+--
+ALTER TABLE `registeruser` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
